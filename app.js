eedbbf980272c4fca42c72220ee1bbd9728e7c84
beemedoblev <tomi.wiersch@gmail.com>
2020-12-20 14:56:50 -0300

    iniciando el html

--- app.js
+++ app.js
@@ -9,3 +9,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname ,'views/index.html'));
 });
 
+app.listen(3000, () => { 
+    console.log("=====================");
+    console.log("http://localhost:3000");
+    console.log("=====================");
+    }
+);
