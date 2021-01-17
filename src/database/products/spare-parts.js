2a24900eb2a400310e923c614bfeac5c2cc91e1f
seba-cod <sebastiankoleff@gmail.com>
2021-01-17 03:56:21 -0300

    agregados unos productos

---
+++ src/database/products/spare-parts.js
@@ -0,0 +1,33 @@
+module.exports = [
+    {
+        id: 'ssskkd-88990',
+        name: 'Pata de motor New Civic 2017',
+        brand: 'honda',
+        desciption: 'Pata de motor original New Civic',
+        piecenumber: '130A',
+        original: true,
+        price: '18.500',
+        vendor: '12321po531fa' /* id del vendedor */
+    },
+    {
+        id: 'ssskkd-88239',
+        name: 'Pata de motor New Civic',
+        brand: 'TWD',
+        desciption: 'Pata de motor simil original New Civic 06-11',
+        piecenumber: '130T',
+        original: false,
+        price: '6.500',
+        vendor: '12321po531fa' /* id del vendedor */
+    },
+    {
+        id: 'kkkfow-12679',
+        name: 'Masa ruleman rueda delantera Vento',
+        brand: 'vw',
+        desciption: 'Masa ruleman rueda delantera VW Vento 2011',
+        piecenumber: 'A93K2',
+        original: false,
+        made: 'Brasil',
+        price: '9.800',
+        vendor: '85936po562fd' /* id del vendedor */
+    }
+];
