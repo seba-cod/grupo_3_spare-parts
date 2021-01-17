d288cdc8335f1890248d97f0681860d6b8e57ec2
seba-cod <sebastiankoleff@gmail.com>
2021-01-17 04:06:44 -0300

    unos datos con mas onda

--- src/database/products/spare-parts.js
+++ src/database/products/spare-parts.js
@@ -1,33 +1,57 @@
 module.exports = [
     {
+        /* Datos auto generados */
         id: 'ssskkd-88990',
+        vendor: '12321po531fa',
+        /* Datos obligatorios */
         name: 'Pata de motor New Civic 2017',
-        brand: 'honda',
-        desciption: 'Pata de motor original New Civic',
-        piecenumber: '130A',
-        original: true,
         price: '18.500',
-        vendor: '12321po531fa' /* id del vendedor */
+        desciption: 'Pata de motor original New Civic',
+        categorie: 'motor',
+        quantity: '35',
+        /* Datos optativos */
+        brand: 'honda',
+        original: true,
+        secondbrand: false,
+        piecenumber: '130A',
+        carBrand: 'Honda',
+        carModel: 'Civic',
+        carYear: '2017',
     },
     {
+        /* Datos auto generados */
         id: 'ssskkd-88239',
+        vendor: '12321po531fa',
+        /* Datos obligatorios */
         name: 'Pata de motor New Civic',
-        brand: 'TWD',
+        price: '6.500',
         desciption: 'Pata de motor simil original New Civic 06-11',
+        categorie: 'motor',
+        quantity: '8',
+        /* Datos optativos */
+        brand: 'TWD',
         piecenumber: '130T',
         original: false,
-        price: '6.500',
-        vendor: '12321po531fa' /* id del vendedor */
+        secondbrand: true,
     },
     {
+        /* Datos auto generados */
         id: 'kkkfow-12679',
+        vendor: '85936po562fd',
+        /* Datos obligatorios */
         name: 'Masa ruleman rueda delantera Vento',
-        brand: 'vw',
+        price: '9.800',
         desciption: 'Masa ruleman rueda delantera VW Vento 2011',
+        categorie: 'exterior',
+        quantity: '12',
+        /* Datos optativos */
+        brand: 'vw',
         piecenumber: 'A93K2',
         original: false,
+        secondbrand: true,
         made: 'Brasil',
-        price: '9.800',
-        vendor: '85936po562fd' /* id del vendedor */
+        carBrand: 'VW',
+        carModel: 'Vento',
+        carYear: '2011',
     }
 ];
