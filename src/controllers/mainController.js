5a3a00fbffb15454c8f20d60b1645e3030b90866
seba-cod <sebastiankoleff@gmail.com>
2021-01-18 20:42:59 -0300

    pruebas con el header

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -5,7 +5,10 @@ module.exports = {
         res.render(path.resolve('./src/views/index.ejs'));
     },
     login: (req,res) =>{
-        res.render(path.resolve('./src/views/login.ejs'))
+        res.render(path.resolve('./src/views/login.ejs'));
+    },
+    header: (req, res) => {
+        res.render(path.resolve('./src/views/partials/header.ejs'));
     }
     // about: (req, res) => {
     //     res.render(path.resolve('./src/views/about.ejs'));
--- src/router/mainRouter.js
+++ src/router/mainRouter.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const mainController = require('../controllers/mainController');
 
 router.get(['/home', '/', ''], mainController.index);
-router.get(['/login','/iniciarsesion'],mainController.login)
+router.get(['/login','/iniciarsesion'],mainController.login);
 // router.get('/about', mainController.about);
+router.get('/header', mainController.header);
+
 
 module.exports = router;
