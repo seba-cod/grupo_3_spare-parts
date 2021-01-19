1a31e3a839bff54cf9bb1065131703b7f6cf9e78
seba-cod <sebastiankoleff@gmail.com>
2021-01-19 03:42:55 -0300

    to lower case los nombres y correcta implementacion de controladores

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,11 +1,8 @@
-const path = require('path');
-
 module.exports = {
     detail: (req, res) => {
-        res.render(path.resolve('./src/views/productDetail.ejs'));
+        res.render('productdetail');
     },
     checkout: (req, res) => {
-        res.render(path.resolve('./src/views/cart.ejs'));
+        res.render('cart');
     },
-
 };
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,9 @@
-const path = require('path');
-
 module.exports = {
     login: (req, res) => {
-        res.render(path.resolve('./src/views/login.ejs'));
+        res.render('login');
     },
     register: (req, res) => {
-        res.render(path.resolve('./src/views/register.ejs'));
+        res.render('register');
     },
 
 };
