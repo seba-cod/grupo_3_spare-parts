46478ac4d0ec00a17b69c6bee5bae031bee00e3b
seba-cod <sebastiankoleff@gmail.com>
2021-02-09 00:21:59 -0300

    formulario de creacion de producto funcionando

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const path = require('path');
+const path = require ('path');
 const jsonTable = require('../database/jsonTable');
 const products = jsonTable('spareparts');
 
@@ -7,7 +7,7 @@ module.exports = {
     detail: (req, res) => {
         // Solicito todos los productos utilizando el método all() dentro del objeto products, que trae la función de trabajos s/json.
         let allProducts = products.all();
-        res.render('product', {allProducts});
+        res.render('product', { allProducts });
     },
     checkout: (req, res) => {
         res.render('cart');
@@ -16,16 +16,37 @@ module.exports = {
         res.render('sell');
     },
     createproduct: (req, res) => {
-        // req.body.name
-        // req.body.description
-        // req.body.price
-        // req.body.categorie
-        // req.body.quantity
-        // //img name_ image
-        // req.body.original
-        // req.body.piecenumber
-        // req.body.carBrand
-        // req.body.carModel
-        // req.body.carYear
+        let newProduct = {
+            //vendor: req.session.user.id, // linkeo al vendedor
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+            categorie: req.body.categorie,
+            quantity: req.body.quantity,
+            //img name_ image // que dato tomar de la imágen para mostrarla?
+            original: "",
+            piecenumber: "",
+            carBrand: "",
+            carModel: "",
+            carYear: "",
+        };
+        if (req.body.original != "") {
+            newProduct.original = req.body.original
+        };
+        if (req.body.piecenumber != "") {
+            newProduct.piecenumber = req.body.piecenumber
+        };
+        if (req.body.carBrand != "") {
+            newProduct.carBrand = req.body.carBrand
+        };
+        if (req.body.carModel != "") {
+            newProduct.carModel = req.body.carModel
+        };
+        if (req.body.carYear != "") {
+            newProduct.carYear = req.body.carYear
+        };
+        products.create(newProduct);
+        res.send(newProduct);
+        
     }
-};
+}
