813705503ba99346224fc20102e3e75809f4ba11
seba-cod <sebastiankoleff@gmail.com>
2021-02-27 05:14:33 -0300

    agregado contacto

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -4,5 +4,8 @@ module.exports = {
     },
     about: (req, res) => {
         res.render('about');
+    },
+    contact: (req, res) => {
+        res.render('contact');
     }
 };
--- src/router/mainRouter.js
+++ src/router/mainRouter.js
@@ -4,5 +4,5 @@ const mainController = require('../controllers/mainController');
 
 router.get(['/home', '/', ''], mainController.index);
 router.get(['/about', '/sobre'], mainController.about);
-
+router.get(['/contact', '/contacto'], mainController.contact);
 module.exports = router;
