f32e01c11cbbdcf36bbb213c2f676d490a87cf2d
seba-cod <sebastiankoleff@gmail.com>
2021-04-17 02:19:46 -0300

    hasheadas las psw de usuarios de seed

--- database/seeders/20210415211353-insert-users.js
+++ database/seeders/20210415211353-insert-users.js
@@ -7,19 +7,19 @@ module.exports = {
         user_name:'pedro1',
         first_name:'jose',
         last_name:'perez',
-        email:'asda@asdad.com',
+        email:'prueba1@a.com',
         address:'calle siempre viva 1234',
-        password:'111111',
+        password:'$2y$10$OOlHuF/clX7zFWJ6BQ6sFOB5SOXGwu955hXZSdHOcFIvlgOQyX6sq',
         avatar:'',
-        admin:'false'
+        admin:'true'
       },
       {
         user_name:'rapaz2',
         first_name:'ramiro',
         last_name:'perez',
-        email:'asda@asdad.com',
-        address:'calle siempre viva 1234',
-        password:'222222',
+        email:'prueba2@a.com',
+        address:'calle maso viva 1234',
+        password:'$2y$10$G7Ir0TA/5.OMdP9UBfYrl.67CqsuzxMNwVppfxOxzaf55DBUjwp56',
         avatar:'',
         admin:'false'
       },
@@ -27,9 +27,9 @@ module.exports = {
         user_name:'vivirum3',
         first_name:'santiago',
         last_name:'perez esquivel',
-        email:'asda@asdad.com',
-        address:'calle siempre viva 1234',
-        password:'333333',
+        email:'prueba3@a.com',
+        address:'calle muerta 1234',
+        password:'$2y$10$KSxC/m9Y1.haFo9SG9iL5.bi/13R1Imr4aKggCh7QRzUZ/3mgTK8.',
         avatar:'',
         admin:'false'
       },
