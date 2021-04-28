6b08a43d8205c26f8de11da9d10554733063ebfd
seba-cod <sebastiankoleff@gmail.com>
2021-04-28 20:19:54 -0300

    db act

--- spare-parts-final-statement.sql
+++ spare-parts-final-statement.sql
@@ -1,38 +1,76 @@
-SELECT * FROM `spare-parts`.users;CREATE TABLE `cart` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-04-2021 a las 01:17:47
+-- Versión del servidor: 10.4.17-MariaDB
+-- Versión de PHP: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `spare-parts`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
   `user` int(11) NOT NULL,
   `createdAt` datetime DEFAULT NULL,
   `updatedAt` datetime DEFAULT NULL,
-  `deletedAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idx_cart__user` (`user`),
-  CONSTRAINT `fk_cart__user` FOREIGN KEY (`user`) REFERENCES `users` (`id`)
+  `deletedAt` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cart_product`
+--
 
 CREATE TABLE `cart_product` (
   `cart` int(11) NOT NULL,
   `product` int(11) NOT NULL,
   `createdAt` datetime DEFAULT NULL,
   `updatedAt` datetime DEFAULT NULL,
-  `deletedAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`cart`,`product`),
-  KEY `idx_cart_product` (`product`),
-  CONSTRAINT `fk_cart_product__cart` FOREIGN KEY (`cart`) REFERENCES `cart` (`id`),
-  CONSTRAINT `fk_cart_product__product` FOREIGN KEY (`product`) REFERENCES `products` (`id`)
+  `deletedAt` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categories`
+--
+
 CREATE TABLE `categories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   `createdAt` datetime DEFAULT NULL,
   `updatedAt` datetime DEFAULT NULL,
-  `deletedAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
+  `deletedAt` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `order`
+--
+
 CREATE TABLE `order` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `state` varchar(255) NOT NULL,
   `date_created` date DEFAULT NULL,
   `total_price` decimal(12,2) DEFAULT NULL,
@@ -40,29 +78,33 @@ CREATE TABLE `order` (
   `user` int(11) NOT NULL,
   `createdAt` datetime DEFAULT NULL,
   `updatedAt` datetime DEFAULT NULL,
-  `deletedAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idx_order__user` (`user`),
-  CONSTRAINT `fk_order__user` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE CASCADE
+  `deletedAt` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `order_item`
+--
+
 CREATE TABLE `order_item` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `price` varchar(255) NOT NULL,
   `product` int(11) NOT NULL,
   `order` int(11) NOT NULL,
   `createdAt` datetime DEFAULT NULL,
   `updatedAt` datetime DEFAULT NULL,
-  `deletedAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idx_orderitem__order` (`order`),
-  KEY `idx_orderitem__product` (`product`),
-  CONSTRAINT `fk_orderitem__order` FOREIGN KEY (`order`) REFERENCES `order` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `fk_orderitem__product` FOREIGN KEY (`product`) REFERENCES `products` (`id`) ON DELETE CASCADE
+  `deletedAt` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `products`
+--
+
 CREATE TABLE `products` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `user` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `price` decimal(2,2) NOT NULL,
@@ -78,16 +120,17 @@ CREATE TABLE `products` (
   `category` int(11) NOT NULL,
   `createdAt` datetime DEFAULT NULL,
   `updatedAt` datetime DEFAULT NULL,
-  `deletedAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idx_product__category` (`category`),
-  KEY `idx_product__user` (`user`),
-  CONSTRAINT `fk_product__category` FOREIGN KEY (`category`) REFERENCES `categories` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `fk_product__user` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE CASCADE
+  `deletedAt` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `user_name` varchar(20) NOT NULL,
   `first_name` varchar(25) NOT NULL,
   `last_name` varchar(30) NOT NULL,
@@ -98,7 +141,149 @@ CREATE TABLE `users` (
   `admin` tinyint(1) DEFAULT NULL,
   `createdAt` datetime DEFAULT NULL,
   `updatedAt` datetime DEFAULT NULL,
-  `deletedAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `user_name` (`user_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+  `deletedAt` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `user_name`, `first_name`, `last_name`, `email`, `address`, `password`, `avatar`, `admin`, `createdAt`, `updatedAt`, `deletedAt`) VALUES
+(1, 'admin', 'test1', 'test2', 'test@a.com', 'asdasd', '$2a$10$UQNzzV2ieKNuX6h6tmmWCOY8NGHBJeeIA1l2LtfA3Q1L0eutv0vLC', 'user-1619570428891img.png', NULL, '2021-04-28 00:40:29', '2021-04-28 00:40:29', NULL);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_cart__user` (`user`);
+
+--
+-- Indices de la tabla `cart_product`
+--
+ALTER TABLE `cart_product`
+  ADD PRIMARY KEY (`cart`,`product`),
+  ADD KEY `idx_cart_product` (`product`);
+
+--
+-- Indices de la tabla `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indices de la tabla `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_order__user` (`user`);
+
+--
+-- Indices de la tabla `order_item`
+--
+ALTER TABLE `order_item`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_orderitem__order` (`order`),
+  ADD KEY `idx_orderitem__product` (`product`);
+
+--
+-- Indices de la tabla `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_product__category` (`category`),
+  ADD KEY `idx_product__user` (`user`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_name` (`user_name`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `order`
+--
+ALTER TABLE `order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `order_item`
+--
+ALTER TABLE `order_item`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `fk_cart__user` FOREIGN KEY (`user`) REFERENCES `users` (`id`);
+
+--
+-- Filtros para la tabla `cart_product`
+--
+ALTER TABLE `cart_product`
+  ADD CONSTRAINT `fk_cart_product__cart` FOREIGN KEY (`cart`) REFERENCES `cart` (`id`),
+  ADD CONSTRAINT `fk_cart_product__product` FOREIGN KEY (`product`) REFERENCES `products` (`id`);
+
+--
+-- Filtros para la tabla `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `fk_order__user` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `order_item`
+--
+ALTER TABLE `order_item`
+  ADD CONSTRAINT `fk_orderitem__order` FOREIGN KEY (`order`) REFERENCES `order` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_orderitem__product` FOREIGN KEY (`product`) REFERENCES `products` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `fk_product__category` FOREIGN KEY (`category`) REFERENCES `categories` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_product__user` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
